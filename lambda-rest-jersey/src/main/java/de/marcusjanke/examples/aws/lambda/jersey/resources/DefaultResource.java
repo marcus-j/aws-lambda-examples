ffb69425e5a3b162e7471e0ccd42a585a4d172c3
marcus-j <18111840+marcus-j@users.noreply.github.com>
2019-02-25 20:33:32 +0100

    reduces boiler plate code

--- lambda-rest-jersey/src/main/java/de/marcusjanke/examples/aws/lambda/jersey/resources/DefaultResource.java
+++ lambda-rest-jersey/src/main/java/de/marcusjanke/examples/aws/lambda/jersey/resources/DefaultResource.java
@@ -3,13 +3,14 @@ package de.marcusjanke.examples.aws.lambda.jersey.resources;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import de.marcusjanke.examples.aws.lambda.jersey.services.MessageService;
+
+import static javax.ws.rs.core.MediaType.TEXT_PLAIN;
 
 /**
  * Sample resources
@@ -30,7 +31,7 @@ public class DefaultResource {
 	 * @return default response
 	 */
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
+    @Produces(TEXT_PLAIN)
     public Response getDefaultResponse() {
         return Response.ok(messageService.getMessage()).build();
     }
